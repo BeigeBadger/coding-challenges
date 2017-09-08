1427b58df60b8a31eb021fb64cbd59e05ad27914
Matt Stannett <matt.stannett@gmail.com>
2017-09-08 22:02:18 +1200

    Add solution for question 1.1 with additional data structures

---
+++ Interviews/Arrays and Strings/1.1_IsUnique_AdditionalStructures/1.1_IsUnique_AdditionalStructures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._1_IsUnique_AdditionalStructures
+{
+	internal class Program
+	{
+		/// <summary>
+		/// Is Unique: Implement an algorithm to determine if a string has all unique characters.
+		/// </summary>
+		/// <param name="args"></param>
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your input string");
+
+			string inputString = "";
+
+			do
+			{
+				inputString = Console.ReadLine();
+			}
+			while (string.IsNullOrWhiteSpace(inputString));
+
+			Console.WriteLine(GenerateOutcome(inputString));
+			Console.ReadLine();
+		}
+
+		private static string GenerateOutcome(string inputString)
+		{
+			char[] allInputedCharacters = inputString.ToCharArray();
+			char[] distinctCharacters = allInputedCharacters.Distinct().ToArray();
+
+			int totalNumberOfCharacters = allInputedCharacters.Length;
+			int numberOfDistinctCharacters = distinctCharacters.Length;
+
+			bool areAllCharactersUnique = totalNumberOfCharacters == numberOfDistinctCharacters;
+
+			return $"{(areAllCharactersUnique ? "Only unique characters are contained in" : "There are duplicated characters in")} '{inputString}'.";
+		}
+	}
+}
