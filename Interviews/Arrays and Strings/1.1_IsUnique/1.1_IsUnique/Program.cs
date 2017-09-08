6358bc5d60c8e40cd6d165842596822393192da0
Matt Stannett <matt.stannett@gmail.com>
2017-09-08 22:41:31 +1200

    Split result and output message functionality to make more modular

--- Interviews/Arrays and Strings/1.1_IsUnique/1.1_IsUnique/Program.cs
+++ Interviews/Arrays and Strings/1.1_IsUnique/1.1_IsUnique/Program.cs
@@ -5,6 +5,9 @@ namespace _1._1_IsUnique
 {
 	internal class Program
 	{
+		/// <summary>
+		/// Is Unique: Implement an algorithm to determine if a string has all unique characters.
+		/// </summary>
 		private static void Main(string[] args)
 		{
 			Console.WriteLine("Please enter your input string");
@@ -17,14 +20,15 @@ namespace _1._1_IsUnique
 			}
 			while (string.IsNullOrWhiteSpace(inputString));
 
-			Console.WriteLine(GenerateOutcomeUsingAdditionalStructures(inputString));
+			bool areAllCharactersUnique = IsUniqueUsingLinq(inputString);
+
+			string outcomeMessage = $"{(areAllCharactersUnique ? "Only unique characters are contained in" : "There are duplicated characters in")} '{inputString}'.";
+
+			Console.WriteLine(outcomeMessage);
 			Console.ReadLine();
 		}
 
-		/// <summary>
-		/// Is Unique: Implement an algorithm to determine if a string has all unique characters.
-		/// </summary>
-		private static string GenerateOutcomeUsingAdditionalStructures(string inputString)
+		private static bool IsUniqueUsingLinq(string inputString)
 		{
 			char[] allInputedCharacters = inputString.ToCharArray();
 			char[] distinctCharacters = allInputedCharacters.Distinct().ToArray();
@@ -32,9 +36,7 @@ namespace _1._1_IsUnique
 			int totalNumberOfCharacters = allInputedCharacters.Length;
 			int numberOfDistinctCharacters = distinctCharacters.Length;
 
-			bool areAllCharactersUnique = totalNumberOfCharacters == numberOfDistinctCharacters;
-
-			return $"{(areAllCharactersUnique ? "Only unique characters are contained in" : "There are duplicated characters in")} '{inputString}'.";
+			return totalNumberOfCharacters == numberOfDistinctCharacters;
 		}
 	}
 }
